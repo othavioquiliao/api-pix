33febe63130c374aca51d32a98eb10355628b956
othavioquiliao <othavioaqa@gmail.com>
2023-10-12 15:59:15 -0300

    Dia 13

---
+++ src/lib/components/Card/Card.svelte
@@ -0,0 +1,10 @@
+<div class="card w-2/4 h-2/4 flex justify-between p-5">
+	<div class="card w-1/2">
+		Basic
+		<a class="block card card-hover p-4" href="/elements/cards">Hover Me!</a>
+	</div>
+	<div class="card w-1/2">
+		Basic
+		<a class="block card card-hover p-4" href="/elements/cards">Hover Me!</a>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Card from '$lib/components/Card/Card.svelte';
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center flex-col">
+	<h2 class="h2 font-extrabold">Consumindo API PIX</h2>
+	<Card />
+</div>
